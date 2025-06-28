4ab8d334040a5c35174273a36b32765b4df27187
AhmedBasha02 <ahmed.bashaa0220@gmail.com>
2025-06-29 02:57:13 +0300

    Add project files.

---
+++ SessionFour/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SessionFour_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+// use do while when you can not know how itteration can you loop 
+// if you kno how itteration will happen then use while 
+// do while used when you know or didn't know number of itteration
+
+// if you have 1 condtion and you know that will check abot this condition 1 try only then use if condition
+// not put condtion that you nned to chek for 1 try only not put in while or do while
+// make it alone in if condition then use while or  do while 
