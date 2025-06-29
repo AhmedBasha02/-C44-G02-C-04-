da223ec0eecc52f0e128b8135e029332cb97ead7
AhmedBasha02 <ahmed.bashaa0220@gmail.com>
2025-06-29 03:29:14 +0300

    Write Task & Questions in Regions First

--- SessionFour_Assignment/Program.cs
+++ SessionFour_Assignment/Program.cs
@@ -4,7 +4,231 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Task
+
+            /*
+             Student Age Checker (Using Relational Patterns with Switch)
+             Let the user enter a student's age.
+             Use switch with relational pattern to check:
+             
+             If age > 25 : print "Older Student"
+             If age between 18–25 : print "University Student"     
+             If age < 18 : print "School Student"
+                   
+             Use switch expression / relational pattern / when keyword
+            */
+
+            #endregion
+
+            #region 1- Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+            /*
+             Example (1)
+             Input: 12 
+             Output: Yes
+             Example (2)
+             Input: 9 
+             Output: No
+             */
+            #endregion
+
+            #region 2- Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+            /*
+             Example (1)
+             Input: -5
+             Output: negative
+             Example (2)
+             Input: 10
+             Output: positive
+             */
+            #endregion
+
+            #region 3- Write a program that takes 3 integers from the user then prints the max element and the min element.
+            /*
+             Example (1)
+             Input:7,8,5
+             Output:
+             max element = 8
+             min element = 5
+             Example (2)
+             Input: 3 6 9
+             Outputs:
+             Max element = 9
+             Min element = 3
+             */
+            #endregion
+
+            #region 4- Write a program that allows the user to insert an integer number then check If a number is even or odd.
+            #endregion
+
+            #region 5- Write a program that takes character from the user then if it is a vowel chars (a,e,I,o,u) then print (vowel) otherwise print (consonant).
+            /*
+             Example (1)
+             Input: O
+             Output: vowel
+             Example (2)
+             Input: b
+             Output: Consonant
+             */
+            #endregion
+
+            #region 6- Write a program that allows the user to insert an integer then print all numbers between 1 to that number.
+            /*
+             Example 
+             Input: 5
+             Output: 1, 2, 3, 4, 5
+             */
+            #endregion
+
+            #region 7- Write a program that allows the user to insert an integer then print a multiplication table up to 12.
+
+            /*
+             Example
+             Input: 5
+             Outputs: 5 10 15 20 25 30 35 40 45 50 55 60
+            */
+            #endregion
+
+            #region 8- Write a program that allows to user to insert number then print all even numbers between 1 to this number
+            /*
+             Example:
+             Input: 15
+             Output: 2 4 6 8 10 12 14
+             */
+            #endregion
+
+            #region 9- Write a program that takes two integers then prints the power.
+            /*
+             Example:
+             Input: 4 3
+             Output: 64
+             Hint: how to calculate 4^3 = 4 * 4 * 4 =64          
+             */
+            #endregion
+
+            #region 10- Write a program to enter marks of five subjects and calculate total, average and percentage.
+            /*
+             Example
+             Input: - Enter Marks of five subjects: 95 76 58 90 89
+             Output: Total marks = 408
+                     Average Marks = 81
+                      Percentage = 81
+             */
+            #endregion
+
+            #region 11- Write a program to input the month number and print the number of days in that month.
+            /*
+             Example
+             Input: Month Number: 1
+             Output: Days in Month: 31
+             */
+            #endregion
+
+            #region 12- Write a program to create a Simple Calculator.
+            #endregion
+
+            #region 13- Write a program to allow the user to enter a string and print the REVERSE of it.
+            #endregion
+
+            #region 14- Write a program to allow the user to enter int and print the REVERSED of it.
+            #endregion
+
+            #region 15- Write a program in C# Sharp to find prime numbers within a range of numbers.
+            /*
+             Test Data :
+             Input starting number of range: 1
+             Input ending number of range : 50
+             
+             Expected Output :
+             The prime number between 1 and 50 are :
+             2 3 5 7 11 13 17 19 23 29 31 37 41 43 47
+             */
+            #endregion
+
+            #region 16- Write a program in C# Sharp to convert a decimal number into binary without using an array.
+            /*
+             Test Data :
+             Enter a number to convert : 25
+             Expected Output :
+             The Binary of 25 is 11001.
+             */
+            #endregion
+
+            #region 17- Create a program that asks the user to input three points (x1, y1), (x2, y2), and (x3, y3), and determines whether these points lie on a single straight line.
+            #endregion
+
+            #region 18- Within a company, the efficiency of workers is evaluated based on the duration required to complete a specific task. A worker's efficiency level is determined as follows: -If the worker completes the job within 2 to 3 hours, they are considered highly efficient.- If the worker takes 3 to 4 hours, they are instructed to increase their speed.- If the worker takes 4 to 5 hours, they are provided with training to enhance their speed.- If the worker takes more than 5 hours, they are required to leave the company.To calculate the efficiency of a worker, the time taken for the task is obtained via user input from the keyboard.
+            #endregion
+
+            #region 19- Write a program that prints an identity matrix using for loop, in other words takes a value n from the user and shows the identity table of size n * n.
+            #endregion
+
+            #region 20- Write a program in C# Sharp to find the sum of all elements of the array.
+            #endregion
+
+            #region 21- Write a program in C# Sharp to merge two arrays of the same size sorted in ascending order.
+            #endregion
+
+            #region 22- Write a program in C# Sharp to count the frequency of each element of an array.
+            #endregion
+
+            #region 23- Write a program in C# Sharp to find maximum and minimum element in an array
+            #endregion
+
+            #region 24- Write a program in C# Sharp to find the second largest element in an array.
+            #endregion
+
+            #region 25- Consider an Array of Integer values with size N, having values as in this Example
+            /*
+             7	   0	0	0	0	5	6	7	5	0	7	5	3
+             
+             write a program find the longest distance between Two equal cells. In this example. The distance is measured by the number Of cells- for example, the distance between the first and the fourth cell is 2 (cell 2 and cell 3).
+             
+             In the example above, the longest distance is between the first 7 and the
+             10th 7, with a distance of 8 cells, i.e. the number of cells between the 1st
+             And the 10th 7s.
+             
+             Note:
+             - Array values will be taken from the user
+             - If you have input like 1111111 then the distance is the number of
+             Cells between the first and the last cell.
+             */
+            #endregion
+
+            #region 26- Given a list of space separated words, reverse the order of the words.
+            /*
+             Input: this is a test		Output: test a is this
+             Input: all your base		Output: base your all
+             Input: Word			Output: Word
+             Note : 
+             Check the Split Function (Member in String Class) Output will be a Single Console.WriteLine Statement
+             */
+            #endregion
+
+            #region 27- Write a program to create two multidimensional arrays of same size. Accept value from user and store them in first array. Now copy all the elements of first array on second array and print second array.
+            #endregion
+
+            #region 28- Write a Program to Print One Dimensional Array in Reverse Order
+            #endregion
+
+            #region 29- Write a program that uses the ternary operator to check if the temperature is too hot, too cold, or just good. Assign the result in a variable then display the result. Assume that below 10 degrees is "Just Cold", above 30 degrees is "Just Hot", and anything else is "Just Good".
+            #endregion
+
+            #region 30- What will be the output of the C# code given below?
+            /*
+             int num = 1, z = 5;
+
+             if (!(num <= 0))
+                 Console.WriteLine( ++num + z++ + " " + ++z ); 
+             else
+                 Console.WriteLine( --num + z-- + " " + --z ); 
+             
+             a.	5 6
+             b.	6 5
+             c.	6 6
+             d.	7 7
+             */
+            #endregion
+
         }
     }
 }
